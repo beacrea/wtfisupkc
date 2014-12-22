f6c96a59d5b48e2cc498f7e2f1a2381f842184a9
Coty Beasley <cbeasley@epiqsystems.com>
2014-12-22 17:41:54 -0600

    Got the script to pull data to console. Added a gitignore.

--- js/app.js
+++ js/app.js
@@ -1,81 +1,41 @@
-var your_url = 'http://www.sprintcenter.com/events';
+"use strict";
 
-// jquery.xdomainajax.js  ------ from padolsey
+var DATA;
 
-jQuery.ajax = (function(_ajax){
-
-    var protocol = location.protocol,
-        hostname = location.hostname,
-        exRegex = RegExp(protocol + '//' + hostname),
-        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
-        query = 'select * from html where url="{URL}" and xpath="*"';
-
-    function isExternal(url) {
-        return !exRegex.test(url) && /:\/\//.test(url);
-    }
-
-    return function(o) {
-
-        var url = o.url;
-
-        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
-
-            // Manipulate options so that JSONP-x request is made to YQL
-
-            o.url = YQL;
-            o.dataType = 'json';
-
-            o.data = {
-                q: query.replace(
-                    '{URL}',
-                    url + (o.data ?
-                    (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
-                        : '')
-                ),
-                format: 'xml'
-            };
-
-            // Since it's a JSONP request
-            // complete === success
-            if (!o.success && o.complete) {
-                o.success = o.complete;
-                delete o.complete;
-            }
-
-            o.success = (function(_success){
-                return function(data) {
-
-                    if (_success) {
-                        // Fake XHR callback.
-                        _success.call(this, {
-                            responseText: data.results[0]
-                                // YQL screws with <script>s
-                                // Get rid of them
-                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
-                        }, 'success');
-                    }
-
-                };
-            })(o.success);
-
+function kimonoCallback(data) {
+    DATA = data;
+    var events = data.results.events;
+    for (var event in events) {
+        if (event === '0') {
+            console.log('The first event is ' +
+                events[event].title.text +
+                ' on '  +
+                events[event].date +
+                ' at ' +
+                events[event].time +
+                '.\n'
+            );
+            console.log('=====================');
+            console.log('Other events include:\n');
+            console.log('=====================');
+        } else if (JSON.parse(event) < 3) {
+            console.log(
+                events[event].title.text +
+                ' on '  +
+                events[event].date +
+                ' at ' +
+                events[event].time +
+                '.\n'
+            );
         }
-
-        return _ajax.apply(this, arguments);
-
-    };
-
-})(jQuery.ajax);
-
-
+    }
+}
 
 $.ajax({
-    url: your_url,
-    type: 'GET',
-    success: function(res) {
-        var text = res.responseText;
-        // then you can manipulate your text as you wish
-        $('.raw').html(text).hide();
-        var contentExtract = $('.raw .overview h1');
-        $('.filter').html($('.raw #list.list > .entry:first-child'));
-    }
+    "url":"https://www.kimonolabs.com/api/54g2ek7c?apikey=9KeRaENgXHwPpPilIumKwosuk7wISts0&callback=kimonoCallback",
+    "crossDomain":true,
+    "dataType":"jsonp"
 });
+
+$(document).foundation();
+kimonoCallback();
