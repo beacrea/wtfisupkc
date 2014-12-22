107fbaade2854f6ed6e89d25cb5e1c09b25516cd
Coty Beasley <cbeasley@epiqsystems.com>
2014-12-22 17:53:57 -0600

    modified callback.

--- js/app.js
+++ js/app.js
@@ -2,40 +2,38 @@
 
 var DATA;
 
-function kimonoCallback(data) {
-    DATA = data;
-    var events = data.results.events;
-    for (var event in events) {
-        if (event === '0') {
-            console.log('The first event is ' +
-                events[event].title.text +
-                ' on '  +
-                events[event].date +
-                ' at ' +
-                events[event].time +
-                '.\n'
-            );
-            console.log('=====================');
-            console.log('Other events include:\n');
-            console.log('=====================');
-        } else if (JSON.parse(event) < 3) {
-            console.log(
-                events[event].title.text +
-                ' on '  +
-                events[event].date +
-                ' at ' +
-                events[event].time +
-                '.\n'
-            );
+$.ajax({
+    "url":"https://www.kimonolabs.com/api/54g2ek7c?apikey=9KeRaENgXHwPpPilIumKwosuk7wISts0",
+    "crossDomain":true,
+    "dataType":"jsonp",
+    "success" : function(data) {
+        DATA = data;
+        var events = data.results.events;
+        for (var event in events) {
+            if (event === '0') {
+                console.log('The first event is ' +
+                    events[event].title.text +
+                    ' on '  +
+                    events[event].date +
+                    ' at ' +
+                    events[event].time +
+                    '.\n'
+                );
+                console.log('=====================');
+                console.log('Other events include:\n');
+                console.log('=====================');
+            } else if (JSON.parse(event) < 3) {
+                console.log(
+                    events[event].title.text +
+                    ' on '  +
+                    events[event].date +
+                    ' at ' +
+                    events[event].time +
+                    '.\n'
+                );
+            }
         }
     }
-}
-
-$.ajax({
-    "url":"https://www.kimonolabs.com/api/54g2ek7c?apikey=9KeRaENgXHwPpPilIumKwosuk7wISts0&callback=kimonoCallback",
-    "crossDomain":true,
-    "dataType":"jsonp"
 });
 
 $(document).foundation();
-kimonoCallback();
