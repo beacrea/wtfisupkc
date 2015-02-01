d35aa7617bbb9a1bbd5cf5fb5f61301fa315537c
Coty Beasley <coty@cotybeasley.name>
2015-02-01 16:17:10 -0600

    Removed comment.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,56 @@
 "use strict";
 
-var DATA;
 
+
+
+
+
+/* ==========================================================================
+ $_ANGULAR DEFAULTS ---------------------------------------------------------
+ ========================================================================== */
+
+var app = angular.module('wtfisupkc', []);
+
+
+
+
+
+/* ==========================================================================
+ $_MODEL POPULATION ---------------------------------------------------------
+ ========================================================================== */
+
+// Set App Data Var
+var DATA = {};
+
+// Pull Data From Scraper
 $.ajax({
     "url":"https://www.kimonolabs.com/api/54g2ek7c?apikey=9KeRaENgXHwPpPilIumKwosuk7wISts0",
     "crossDomain":true,
     "dataType":"jsonp",
     "success" : function(data) {
-        DATA = data;
+        console.log('Successfully executed GET...');
+        DATA.sprintCenter = data.results.events;
         var events = data.results.events;
-        for (var event in events) {
-            if (event === '0') {
-                $('h2').append(
-                    events[event].title.text +
-                    ' on '  +
-                    events[event].date +
-                    ' at ' +
-                    events[event].time +
-                    '.\n'
-                );
-            } else if (JSON.parse(event) < 3) {
-                console.log(
-                    events[event].title.text +
-                    ' on '  +
-                    events[event].date +
-                    ' at ' +
-                    events[event].time +
-                    '.\n'
-                );
-            }
-        }
+        console.log('Data saved to global variable...');
+        $('h2').append(
+            events[0].title.text +
+            ' on '  +
+            events[0].date +
+            ' at ' +
+            events[0].time +
+            '.\n'
+        );
         $('.loader').hide();
+        console.log('Filtered and displayed data.');
     }
 });
+
+
+
+
+
+/* ==========================================================================
+ $_APP INITS ----------------------------------------------------------------
+ ========================================================================== */
 
 $(document).foundation();
